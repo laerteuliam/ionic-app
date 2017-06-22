fa2c450f4de3eb219c55e5f7b101cf45fe1a97b5
Laerte Uliam <luliam@ezconet.int.br>
2017-06-22 14:50:44 -0300

    teste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  teste:string = "teste";
   constructor(public navCtrl: NavController) {
 
   }
